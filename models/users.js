6041ad32f84c1fd167fab92c27c5a84498660d32
Mindy D Tran <mindytran98@gmail.com>
2024-01-19 15:46:37 -0500

    make schema for user

--- models/users.js
+++ models/users.js
@@ -1,5 +1,26 @@
 import mongoose from "mongoose";
 
-const usersSchema = new mongoose.Schema({});
+const usersSchema = new mongoose.Schema({
+    name:{
+        type: String
+    },
+    username: {
+        type: String,
+        minLength:3,
+        maxLength: 20,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 20
+    }
+}, {timestamps: true});
 
 export default mongoose.model('User', usersSchema);
