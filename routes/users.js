de46471913077b883e40669862bf989c29b4f901
Mindy D Tran <mindytran98@gmail.com>
2024-01-19 22:09:02 -0500

    create post method

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ import User from "../models/users.js";
 
 const router = new Router();
 
+/**
+ * GET @description get users
+ */
 router.get("/", async (req, res) => {
   try {
     const users = await User.find({});
@@ -12,5 +15,17 @@ router.get("/", async (req, res) => {
     console.log(error.message);
   }
 });
+
+/**
+ * POST @description create user
+ */
+router.post('/', async(req,res)=>{
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 export default router;
