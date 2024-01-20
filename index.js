f8984171816bfc69bbfee68dfb6ff9981f680e12
Mindy D Tran <mindytran98@gmail.com>
2024-01-19 19:00:49 -0500

    add routes and localenv.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-import express from "express";
+import './localEnv.js';
 import {conn} from './db/conn.js'; conn();
+import express from "express";
 import morgan from "morgan";
+
+import usersRoutes from './routes/users.js';
+import productsRoutes from './routes/products.js';
+import ordersRoutes from './routes/orders.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api/users', usersRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/orders', ordersRoutes);
 
 app.get('/', (req,res=>{
     res.send('Welcome to the api');
