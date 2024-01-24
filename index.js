4dbb16f238e9959a32f209c43ddf7a13ad5b6aa1
Mindy D Tran <mindytran98@gmail.com>
2024-01-24 17:11:48 -0500

    add wishlist route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import usersRoutes from './routes/users.js';
 import productsRoutes from './routes/products.js';
 import ordersRoutes from './routes/orders.js';
+import wishListRoutes from './routes/wishLists.js';
 
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/api/users', usersRoutes);
 app.use('/api/products', productsRoutes);
 app.use('/api/orders', ordersRoutes);
+app.use('/api/wishLists', wishListRoutes);
 
 app.get('/', (req,res)=>{
     res.send('Welcome to the api');
