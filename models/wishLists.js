c29a8b23fa41741fc509a39832f90920ca939386
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 22:21:58 -0500

    create wish list model

---
+++ models/wishLists.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const wishlistItem = new mongoose.Schema(
+  {
+    product_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+    },
+  },
+  { _id: false }
+);
+
+const wishListsSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    orderList: {
+      type: [wishlistItem],
+    },
+  },
+);
+
+export default mongoose.model("WishList", wishListsSchema);
